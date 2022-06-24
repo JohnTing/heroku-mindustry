c13231e4d7ecc8b72554af6e79db4a8658624c2e
JohnTing <jot.johnting@gmail.com>
2022-06-24 15:15:56 +0800

    psutil

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import subprocess
 from flask import Flask
 import os
 
+import psutil
+
 token = os.environ.get("token")
+
+
 
 subprocess.Popen(["echo host | java -jar mindustry-server/server.jar"], shell=True)
 subprocess.Popen(["./localtonet authtoken ${token}"], shell=True)
@@ -16,7 +20,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+
+    datastrs = [
+        psutil.cpu_percent(interval=1, percpu=True), 
+        psutil.virtual_memory(), 
+        psutil.swap_memory()
+    ]
+
+
+
+    return "<p>Hello, World!</p>" + "<br/>".join(datastrs)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
